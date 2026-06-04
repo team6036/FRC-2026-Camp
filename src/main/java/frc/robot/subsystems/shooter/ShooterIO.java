028e957077303088c862b70338eb73c71eae6305
zilthric <leo.gelb@yahoo.com>
2026-06-03 21:43:47 -0700

    spotless

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -9,51 +9,56 @@ import frc.robot.constants.ShooterConstants;
 
 public class ShooterIO {
 
-    private final TalonFX bottomLeftMotor = new TalonFX(ShooterConstants.bottomLeftMotorId,  ShooterConstants.bus);
-    private final TalonFX bottomRightMotor = new TalonFX(ShooterConstants.bottomRightMotorId, ShooterConstants.bus);
-    private final TalonFX topLeftMotor = new TalonFX(ShooterConstants.topLeftMotorId,     ShooterConstants.bus);
-    private final TalonFX topRightMotor = new TalonFX(ShooterConstants.topRightMotorId,    ShooterConstants.bus);
+  private final TalonFX bottomLeftMotor =
+      new TalonFX(ShooterConstants.bottomLeftMotorId, ShooterConstants.bus);
+  private final TalonFX bottomRightMotor =
+      new TalonFX(ShooterConstants.bottomRightMotorId, ShooterConstants.bus);
+  private final TalonFX topLeftMotor =
+      new TalonFX(ShooterConstants.topLeftMotorId, ShooterConstants.bus);
+  private final TalonFX topRightMotor =
+      new TalonFX(ShooterConstants.topRightMotorId, ShooterConstants.bus);
 
-    private final VelocityVoltage velocityRequest = new VelocityVoltage(0);
+  private final VelocityVoltage velocityRequest = new VelocityVoltage(0);
 
-    public static class ShooterIOInputs {
-        public double bottomLeftVelocityRPS = 0.0;
-        public double bottomRightVelocityRPS = 0.0;
-        public double topLeftVelocityRPS = 0.0;
-        public double topRightVelocityRPS = 0.0;
-    }
+  public static class ShooterIOInputs {
+    public double bottomLeftVelocityRPS = 0.0;
+    public double bottomRightVelocityRPS = 0.0;
+    public double topLeftVelocityRPS = 0.0;
+    public double topRightVelocityRPS = 0.0;
+  }
 
-    public ShooterIO() {
-        TalonFXConfiguration config = new TalonFXConfiguration();
-        config.Slot0.kP = ShooterConstants.kP;
-        config.Slot0.kI = ShooterConstants.kI;
-        config.Slot0.kD = ShooterConstants.kD;
-        config.Slot0.kS = ShooterConstants.kS;
-        config.Slot0.kV = ShooterConstants.kV;
-        config.Slot0.kA = ShooterConstants.kA;
+  public ShooterIO() {
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    config.Slot0.kP = ShooterConstants.kP;
+    config.Slot0.kI = ShooterConstants.kI;
+    config.Slot0.kD = ShooterConstants.kD;
+    config.Slot0.kS = ShooterConstants.kS;
+    config.Slot0.kV = ShooterConstants.kV;
+    config.Slot0.kA = ShooterConstants.kA;
 
-        bottomLeftMotor.getConfigurator().apply(config);
-        topLeftMotor.getConfigurator().apply(config);
+    bottomLeftMotor.getConfigurator().apply(config);
+    topLeftMotor.getConfigurator().apply(config);
 
-        bottomRightMotor.setControl(new Follower(ShooterConstants.bottomLeftMotorId, MotorAlignmentValue.Opposed));
-        topRightMotor.setControl(new Follower(ShooterConstants.topLeftMotorId, MotorAlignmentValue.Opposed));
+    bottomRightMotor.setControl(
+        new Follower(ShooterConstants.bottomLeftMotorId, MotorAlignmentValue.Opposed));
+    topRightMotor.setControl(
+        new Follower(ShooterConstants.topLeftMotorId, MotorAlignmentValue.Opposed));
+  }
 
-    }
+  public void updateInputs(ShooterIOInputs inputs) {
+    inputs.bottomLeftVelocityRPS = bottomLeftMotor.getVelocity().getValueAsDouble();
+    inputs.bottomRightVelocityRPS = bottomRightMotor.getVelocity().getValueAsDouble();
+    inputs.topLeftVelocityRPS = topLeftMotor.getVelocity().getValueAsDouble();
+    inputs.topRightVelocityRPS = topRightMotor.getVelocity().getValueAsDouble();
+  }
 
-    public void updateInputs(ShooterIOInputs inputs) {
-        inputs.bottomLeftVelocityRPS = bottomLeftMotor.getVelocity().getValueAsDouble();
-        inputs.bottomRightVelocityRPS = bottomRightMotor.getVelocity().getValueAsDouble();
-        inputs.topLeftVelocityRPS = topLeftMotor.getVelocity().getValueAsDouble();
-        inputs.topRightVelocityRPS = topRightMotor.getVelocity().getValueAsDouble();
-    }
+  public void setVelocity(double velocityRPS) {
+    bottomLeftMotor.setControl(velocityRequest.withVelocity(velocityRPS));
+    topLeftMotor.setControl(velocityRequest.withVelocity(velocityRPS));
+  }
 
-    public void setVelocity(double velocityRPS) {
-        bottomLeftMotor.setControl(velocityRequest.withVelocity(velocityRPS));
-        topLeftMotor.setControl(velocityRequest.withVelocity(velocityRPS));
-    }
-
-    public void stop() {
-        bottomLeftMotor.stopMotor();
-        topLeftMotor.stopMotor();
-    }
+  public void stop() {
+    bottomLeftMotor.stopMotor();
+    topLeftMotor.stopMotor();
+  }
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -7,38 +7,38 @@ import frc.robot.util.Logger;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-    public enum Mode {
-        OFF,
-        SHOOTING,
+  public enum Mode {
+    OFF,
+    SHOOTING,
+  }
+
+  private final ShooterIO io;
+  private final ShooterIOInputs inputs = new ShooterIOInputs();
+  private Mode wantedMode = Mode.OFF;
+
+  public ShooterSubsystem(ShooterIO io) {
+    this.io = io;
+  }
+
+  public void setMode(Mode mode) {
+    this.wantedMode = mode;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.log("Subsystems/Shooter/BottomLeftVelocity", inputs.bottomLeftVelocityRPS);
+    Logger.log("Subsystems/Shooter/BottomRightVelocity", inputs.bottomRightVelocityRPS);
+    Logger.log("Subsystems/Shooter/TopLeftVelocity", inputs.topLeftVelocityRPS);
+    Logger.log("Subsystems/Shooter/TopRightVelocity", inputs.topRightVelocityRPS);
+
+    switch (wantedMode) {
+      case SHOOTING:
+        io.setVelocity(ShooterConstants.desiredVelocity);
+        break;
+      case OFF:
+      default:
+        io.stop();
     }
-
-    private final ShooterIO io;
-    private final ShooterIOInputs inputs = new ShooterIOInputs();
-    private Mode wantedMode = Mode.OFF;
-
-    public ShooterSubsystem(ShooterIO io) {
-        this.io = io;
-    }
-
-    public void setMode(Mode mode) {
-        this.wantedMode = mode;
-    }
-
-    @Override
-    public void periodic() {
-        io.updateInputs(inputs);
-        Logger.log("Subsystems/Shooter/BottomLeftVelocity", inputs.bottomLeftVelocityRPS);
-        Logger.log("Subsystems/Shooter/BottomRightVelocity", inputs.bottomRightVelocityRPS);
-        Logger.log("Subsystems/Shooter/TopLeftVelocity", inputs.topLeftVelocityRPS);
-        Logger.log("Subsystems/Shooter/TopRightVelocity", inputs.topRightVelocityRPS);
-
-        switch (wantedMode) {
-            case SHOOTING:
-                io.setVelocity(ShooterConstants.desiredVelocity);
-                break;
-            case OFF:
-            default:
-                io.stop();
-        }
-    }
+  }
 }
