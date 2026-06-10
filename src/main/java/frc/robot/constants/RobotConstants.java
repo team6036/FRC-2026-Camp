06ef6194e2d46aa96ea06ae11e4dfa760ab0759b
zilthric <leo.gelb@yahoo.com>
2026-06-10 12:47:33 -0700

    fix

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -9,61 +9,82 @@ import java.util.Optional;
 
 public class RobotConstants {
 
-  public enum RobotType {
-    CAMP_C,
-    CAMP_D,
-    UNKNOWN
-  }
+    public enum RobotType {
+        CAMP_A,
+        CAMP_B,
+        CAMP_C,
+        CAMP_D,
+        UNKNOWN
+    }
 
-  public enum SwerveModuleType {
-    MK4n_L2,
-    MK5n_L2,
-    MK4i_L2
-  }
+    public enum SwerveModuleType {
+        MK4n_L2,
+        MK5n_L2,
+        MK4i_L2
+    }
 
-  public static final String serialNumber =
-      Robot.isReal() ? RobotController.getSerialNumber() : "SIMULATION";
+    public enum DriveDirection {
+        NORMAL,
+        LEFT,
+        RIGHT
+    }
 
-  public static final RobotType robotType =
-      switch (serialNumber) {
-        case "0251EF71" -> RobotType.CAMP_D;
-        case "0326F27E" -> RobotType.CAMP_C;
-        default -> {
-          if (Robot.isReal()) {
-            new Alert(
-                    "Unknown roboRIO Serial: "
-                        + serialNumber
-                        + ", some functions may not work correctly",
-                    Alert.AlertType.kError)
-                .set(true);
-            yield RobotType.UNKNOWN;
-          } else {
-            yield RobotType.CAMP_D;
-          }
-        }
-      };
+    public static final String serialNumber =
+            Robot.isReal() ? RobotController.getSerialNumber() : "SIMULATION";
 
-  public static final SwerveModuleType swerveModuleType =
-      switch (robotType) {
-        case CAMP_C -> SwerveModuleType.MK4i_L2;
-        case CAMP_D -> SwerveModuleType.MK5n_L2;
-        case UNKNOWN -> SwerveModuleType.MK4n_L2;
-      };
+    public static final RobotType robotType =
+            switch (serialNumber) {
+                case "023FF3E1" -> RobotType.CAMP_A;
+                case "032381B0" -> RobotType.CAMP_B;
+                case "0326F27E" -> RobotType.CAMP_C;
+                case "0251EF71" -> RobotType.CAMP_D;
+                default -> {
+                    if (Robot.isReal()) {
+                        new Alert(
+                                "Unknown roboRIO Serial: "
+                                        + serialNumber
+                                        + ", some functions may not work correctly",
+                                Alert.AlertType.kError)
+                                .set(true);
+                        yield RobotType.UNKNOWN;
+                    } else {
+                        yield RobotType.UNKNOWN;
+                    }
+                }
+            };
 
-  /* CAN Buses */
-  public static final CANBus rio = new CANBus("rio");
+    public static final SwerveModuleType swerveModuleType =
+            switch (robotType) {
+                case CAMP_A -> SwerveModuleType.MK4n_L2;
+                case CAMP_B -> SwerveModuleType.MK4n_L2;
+                case CAMP_C -> SwerveModuleType.MK4i_L2;
+                case CAMP_D -> SwerveModuleType.MK5n_L2;
+                case UNKNOWN -> SwerveModuleType.MK4n_L2;
+            };
 
-  /* Logging */
-  public static final boolean enableLogging = true;
+    public static final DriveDirection driveDirection =
+            switch (robotType) {
+                case CAMP_A -> DriveDirection.LEFT;
+                case CAMP_B -> DriveDirection.LEFT;
+                case CAMP_C -> DriveDirection.LEFT;
+                case CAMP_D -> DriveDirection.LEFT;
+                case UNKNOWN -> DriveDirection.NORMAL;
+            };
 
-  /* Alliance */
-  public static boolean onBlue() {
-    Optional<DriverStation.Alliance> alliance = DriverStation.getAlliance();
-    return alliance.isPresent() && alliance.get() == DriverStation.Alliance.Blue;
-  }
+    /* CAN Buses */
+    public static final CANBus rio = new CANBus("rio");
 
-  public static boolean onRed() {
-    Optional<DriverStation.Alliance> alliance = DriverStation.getAlliance();
-    return alliance.isPresent() && alliance.get() == DriverStation.Alliance.Red;
-  }
+    /* Logging */
+    public static final boolean enableLogging = true;
+
+    /* Alliance */
+    public static boolean onBlue() {
+        Optional<DriverStation.Alliance> alliance = DriverStation.getAlliance();
+        return alliance.isPresent() && alliance.get() == DriverStation.Alliance.Blue;
+    }
+
+    public static boolean onRed() {
+        Optional<DriverStation.Alliance> alliance = DriverStation.getAlliance();
+        return alliance.isPresent() && alliance.get() == DriverStation.Alliance.Red;
+    }
 }
