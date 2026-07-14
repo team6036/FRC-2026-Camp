59cc4ae270eb592cdd03425d241ad5e5d3698b1d
zilthric <leo.gelb@yahoo.com>
2026-07-14 13:44:42 -0700

    leo 2 working robots, b and c

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -33,8 +33,8 @@ public class RobotConstants {
       switch (serialNumber) {
         case "023FF3E1" -> RobotType.CAMP_B;
         case "032381B0" -> RobotType.CAMP_C;
-        case "0326F27E" -> RobotType.CAMP_A;
-        case "0251EF71" -> RobotType.CAMP_B; // rio moved, D was taken apart and moved to bot B
+        case "0326F27E" -> RobotType.CAMP_C;
+        case "0251EF71" -> RobotType.CAMP_D;
         default -> {
           if (Robot.isReal()) {
             new Alert(
@@ -54,7 +54,7 @@ public class RobotConstants {
       switch (robotType) {
         case CAMP_A -> SwerveModuleType.MK4n_L2;
         case CAMP_B -> SwerveModuleType.MK4n_L2;
-        case CAMP_C -> SwerveModuleType.MK4i_L2;
+        case CAMP_C -> SwerveModuleType.MK5n_L2;
         case CAMP_D -> SwerveModuleType.MK5n_L2;
         case UNKNOWN -> SwerveModuleType.MK4n_L2;
       };
